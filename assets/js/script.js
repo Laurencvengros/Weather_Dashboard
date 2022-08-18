d9d86d9a2dc86e6e23333b51fd40097b7218d899
Lauren <l.cvengros@icloud.com>
2022-08-18 16:40:02 -0400

    console log check to see if function works (it does)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,26 @@
 var APIkey = "3ab800fa705dfe5653a465d8d9a9d6ae";
-var currentCity = "";
 
 
-$("#searchBtn").on("click", function(){
-    var city = $("#searchCities").val();
-    currentCity = $("#searchCities").val();
 
-    var APIurl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIkey;
-    fetch(APIurl)
-    .then(function(response){
-        return response.json();
+$("#searchBtn").on("click", function(event){
+event.preventDefault();
+city = $("#searchCities").val();
+getWeather();
     
 })
 
-});
 
+function getWeather(){
+var city = $("#searchCities").val();
+currentCity = $("#searchCities").val();
+
+var APIurl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIkey;
+fetch(APIurl)
+.then(function(response){
+    return response.json();
+})
+.then(function(response){
+console.log(city);
+}
+
+)};
